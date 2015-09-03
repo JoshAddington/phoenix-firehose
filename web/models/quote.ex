a61419992c5919da2d521602f33c12486314ebdb
joshaddington <josh.addington@gmail.com>
2015-09-03 11:03:21 -0400

    Initial commit - base completed

---
+++ web/models/quote.ex
@@ -0,0 +1,20 @@
+defmodule Splurty.Quote do
+  use Ecto.Model
+
+  schema "quotes" do
+    field :saying, :string
+    field :author, :string
+  end
+
+  defmodule Queries do
+    def random do
+      query = Ecto.Adapters.Postgres.query(
+        Repo,
+        "SELECT id, saying, author from quotes ORDER BY RANDOM() LIMIT 1",
+        [])
+      %Postgrex.Result{rows: [row]} = query
+      {id, saying, author} = row
+      %Splurty.Quote{id: id, saying: saying, author: author}
+    end
+  end
+end
